e89501f491a3c905ae843bedcecaa82afc62abac
Sadiq <seeker99157@gmail.com>
2022-11-08 16:27:23 +0100

    lesson 4 done

---
+++ features/notes/NoteList.jsx
@@ -0,0 +1,6 @@
+const NoteList = () => {
+  return (
+    <h1>NoteList</h1>
+  )
+}
+export default NoteList
--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Public from '../components/Public'
 import Login from '../features/auth/Login'
 import DashLayout from '../components/DashLayout'
 import Welcome from '../features/auth/Welcome'
+import NoteList from '../features/notes/NoteList'
+import UserList from '../features/UserList'
 
 function App() {
   return <div>
@@ -11,11 +13,21 @@ function App() {
       <Route path='/' element={<Layout />}>
         <Route index element={<Public />} />
         <Route path='login' element={<Login />} />
+
         <Route path='dash' element={<DashLayout />}>
 
           <Route index element={<Welcome />} />
 
-        </Route>
+          <Route path='notes'>
+            <Route index element={<NoteList />} />
+          </Route>
+
+          <Route path='users'>
+            <Route index element={<UserList />} />
+          </Route>
+
+        </Route> {/* End Dash */}
+
       </Route>
     </Routes>
   </div>
